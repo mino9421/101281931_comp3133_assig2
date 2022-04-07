4de4b4b5186f44dcc34ca1619195faf8ebbf2812
mino9421 <mino9421@gmail.com>
2022-04-07 19:36:00 -0400

    added paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdminViewListingComponent } from './admin-view-listing/admin-view-listing.component';
+import { HistoryComponent } from './history/history.component';
+import { ListingComponent } from './listing/listing.component';
+import { LoginComponent } from './login/login.component';
+import { NewlistComponent } from './newlist/newlist.component';
+import { SearchListingComponent } from './search-listing/search-listing.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '', component: AdminViewListingComponent },
+  { path: '', component: HistoryComponent },
+  { path: '', component: ListingComponent },
+  { path: '', component: LoginComponent },
+  { path: '', component: NewlistComponent },
+  { path: '', component: SearchListingComponent },
+  { path: '', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
